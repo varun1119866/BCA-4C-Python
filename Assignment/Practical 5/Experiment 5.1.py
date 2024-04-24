419a2ca0ae9c996522d61bdce53bbd9366ee26ef
ajaygodara7018 <163568255+ajaygodara7018@users.noreply.github.com>
2024-04-24 19:53:29 +0530

    Update Experiment 5.1.py

--- Assignment/Practical 5/Experiment 5.1.py
+++ Assignment/Practical 5/Experiment 5.1.py
@@ -1,7 +1,7 @@
 #WAP to show the creation and working of lists
 list1 = []
 list1.append(1)
-list1.append("Radhika Adlakha")
+list1.append("Ajay Godara")
 list1.append(3.45)
 list1.append(True)
 list1.append([1,2,3,4,5])
