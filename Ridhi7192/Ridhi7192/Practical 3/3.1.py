53f2cb51d0607a556e1f05e64e7327bf4f43b395
Ridhi <161427129+Ridhi7192@users.noreply.github.com>
2024-04-27 17:52:27 +0530

    Add files via upload

---
+++ Ridhi7192/Ridhi7192/Practical 3/3.1.py
@@ -0,0 +1,7 @@
+print("NAME : Ridhi \n ROLL NO : 2210997192")
+a=int(input("Enter First Number : "))
+b=int(input("Enter Second Number : "))
+if a%b==0:
+    print(a," is fully divisible by ",b)
+else:
+    print(a," is not fully divisible by ",b)
