05da5dbe28ab21893e5b766be6985c79a5cb1f76
Yuvraj_7282 <114151650+Yuvraj-7282@users.noreply.github.com>
2024-04-27 22:29:36 +0530

    yuvraj singh 2210997282

---
+++ Practical 4/4.4.py
@@ -0,0 +1,10 @@
+print("NAME : YUVRAJ SINGH \n ROLL NO : 2201997282")
+# Example of using else with while and break
+num = 10
+while num > 0:
+    print(num)
+    num -= 1
+    if num == 5:
+        break
+else:
+    print("Loop ended normally")
